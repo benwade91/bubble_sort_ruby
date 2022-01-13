ea64e3de72a4af72fec02f5e8dea36ccfb0e9831
benwade91 <70988204+benn925@users.noreply.github.com>
2022-01-13 14:32:07 -0800

    added comments

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,6 +2,8 @@ def bubble_sort(arr)
     sorted = true
     index = 0
     for i in 0...arr.length-1
+        # if first element is larger than the second, 
+        # they're swapped and sorted is set to false
         if arr[i] > arr[i + 1]
             a = arr[i]
             arr[i] = arr[i + 1]
@@ -9,6 +11,8 @@ def bubble_sort(arr)
             sorted = false
         end
     end
+    # if arr is still unsorted, the function is called again, 
+    # passing in the partially sorted array
     if sorted == false
         bubble_sort(arr)
     else
